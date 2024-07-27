3c3a14e07bb0cffbb363cf6831bfbd522a57b1de
suresh <suresh.ragam@icloud.com>
2024-07-27 18:51:48 +0530

    add footer content

--- myapp/src/components/Footer.js
+++ myapp/src/components/Footer.js
@@ -1,10 +1,13 @@
+
 import "./Footer.css"
 
 const Footer = () =>{
     return (
         <footer>
-            <h1>footer</h1>
-            <p>copyright @roshan</p>
+            <h1>Footer</h1>
+            <h2>Working on it</h2>
+            <p>Made with Love</p>
+            <p>copyright@ roshan</p>
         </footer>
     )
 }
