3ec9e609800a5aa5ea4ba3a8d51ffa771b3a7891
suresh <suresh.ragam@icloud.com>
2024-07-27 15:00:38 +0530

    add react files header file

---
+++ myapp/src/components/Header.js
@@ -0,0 +1,18 @@
+const Header = () =>{
+    return (
+        <header>
+            <div>
+                <img src="" alt="logo"/>
+                <h1>Header Title</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
