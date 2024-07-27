ee3742e321fc2ca85aee0344e114fd58344b09a0
suresh <suresh.ragam@icloud.com>
2024-07-27 15:12:41 +0530

    add footer

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
